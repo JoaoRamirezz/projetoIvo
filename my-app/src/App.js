fc4c2d8b22cfcbf3bffafcb614969fad09d66cb7
joaoramirezz <joao.rkonrath@gmail.com>
2023-11-15 15:58:24 -0300

    algumas logicas a mais

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Home from './pages/home/home';
-import { Route, Router, Routes, RouterProvider } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path='/home' element={<Home/>}/>
+      <Route path='/' element={<Home/>}/>
     </Routes>
     </>
   );
--- my-app/src/pages/home/home.js
+++ my-app/src/pages/home/home.js
@@ -8,46 +8,66 @@ import { useState } from 'react';
 
 
 function Home() {
-  const [numbers, setNumbers] = useState([]);
+  const [indexes, setIndexes] = useState([]);
   const [quantity, setQuantity] = useState();
   const [selectedNum, setSelected] = useState();
+  const [numsList, setNums] = useState([]);
+  const [percent, setPercent] = useState();
 
-  function getNumbers() {
-    return (!numbers) ? null : (
+  function getIndexes() {
+    return (!indexes) ? null : (
       <tbody>
-        {numbers.map((item) => {
+        {indexes.map((item) => {
           return (
             <tr>
+              <td>{item.index}</td>
               <td>{item.number}</td>
               <td>{item.repeat}</td>
+              <td>{item.pct}%</td>
             </tr>
           );
         })}
       </tbody>
-    );
+    )
   }
 
-  function SelectNumber(num){
+  function SelectNumber(num) {
+    var splittedNums = num.split(",")
+    var nums = []
 
-    numbers.map((n) => {
-      if(n.number == num){
-        n.repeat += 1
-      }
-    })
+    splittedNums.forEach(nm => {
+      setIndexes(prev => prev.map((n) => {
+        if (n.number === Number(nm)) {
+          n.repeat += 1
+          n.pct *= n.pct
+          nums.push(nm)
+        }
+        else{
+          n.number = Math.floor(Math.random() * 10);
+          n.repeat = 0
+          n.pct = percent
+        }
+        setNums(nums)
+        return n
+      }))
+    });
   }
 
   function ListRender(qtt) {
-    const n = []
+    var n = []
 
     for (let i = 0; i < qtt; i++) {
       const number = Math.floor(Math.random() * 10);
       const obj = {
-        "number" : number,
-        "repeat" : 0
+        "index": i + 1,
+        "number": number,
+        "repeat": 0,
+        "pct": percent
       }
+
       n.push(obj)
     }
-    setNumbers(n)
+    setIndexes(n)
   }
 
   return (
@@ -64,18 +84,27 @@ function Home() {
 
         <div className={style.numbers}>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Control type="number" placeholder="Selecione um numero: " onChange={e => setSelected(e.target.value)} />
+            <Form.Control type="text" placeholder="Selecione um numero: " onChange={e => setSelected(e.target.value)} />
           </Form.Group>
           <Button type="submit" onClick={() => SelectNumber(selectedNum)}>Selecionar Numero</Button>
         </div>
+
+        <div className={style.numbers}>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Control type="text" placeholder="Escreva a Porcentagem: " onChange={e => setPercent((e.target.value)/100)} />
+          </Form.Group>
+        </div>
+
       </div>
       <div className={style.numberslist}>
         <table border="1">
           <tr>
-            <td>Numero</td>
-            <td>Repetido</td>
+            <th>Index</th>
+            <th>Numero</th>
+            <th>Repetido</th>
+            <th>Porcentagem</th>
           </tr>
-            {getNumbers()}
+          {getIndexes()}
         </table>
       </div>
 
