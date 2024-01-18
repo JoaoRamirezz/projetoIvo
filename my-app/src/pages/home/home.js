bf05a4241e65c52060faa3e866b7f5b50ef46250
joaoramirezz <joao.rkonrath@gmail.com>
2024-01-18 19:12:32 -0300

    aprimoramento nas atualizações

--- my-app/src/pages/home/home.js
+++ my-app/src/pages/home/home.js
@@ -12,23 +12,13 @@ function Home() {
   const [quantity, setQuantity] = useState();
   const [selectedNum, setSelected] = useState();
   const [percent, setPercent] = useState();
-  const [indexesList, setList] = useState();
 
 
-  function SearchIndex(num){
+  function SortNumbers(num) {
     setIndexes(prev => prev.map((n) => {
-      if (n.linha === num) {
-        n.number = Math.floor(Math.random() * 10);
-        n.repeat += 1
-        n.pct = 0.01
-      }
-      else {
-        n.number = Math.floor(Math.random() * 10);
-        n.pct += (n.pct * percent)
-      }
       return n
-    }).sort(function(a,b){
-      return b.repeat - a.repeat
+    }).sort(function (a, b) {
+      return a.nonrepeat - b.nonrepeat
     }))
   }
 
@@ -46,6 +36,7 @@ function Home() {
               <td>{a}</td>
               <td>{item.linha}</td>
               <td>{item.repeat}</td>
+              <td>{item.nonrepeat}</td>
               <td>{item.number}</td>
               <td>{item.pct}%</td>
             </tr>
@@ -57,48 +48,40 @@ function Home() {
 
   function SelectNumber(num) {
     var splittedNums = num.split(",")
-    var index = 0
-    
+
     splittedNums.forEach(nm => {
       setIndexes(prev => prev.map((n) => {
-        index +=1
         if (n.number === Number(nm)) {
-          SearchIndex(index)
+            n.repeat += 1
+            n.pct = 0.01
+            n.nonrepeat = 0
         }
         else {
-          n.number = Math.floor(Math.random() * 10);
           n.pct += (n.pct * percent)
+          n.nonrepeat += 1
         }
         return n
       }))
-    });
-    
-    
+    })
+    SortNumbers()
   }
 
   function ListRender(qtt) {
     let n = []
     for (let i = 0; i < qtt; i++) {
-      const number = Math.floor(Math.random() * 10);
+      const number = Math.floor(Math.random() * 25) + 1;
       const obj = {
         "index": i + 1,
         "linha": i + 1,
         "number": number,
         "repeat": 0,
+        "nonrepeat": 0,
         "pct": 0.01
       }
       n.push(obj)
     }
 
-    const listObj ={
-      "number": indexesList.lenght + 1,
-      "list":n
-    }
-
     setIndexes(n)
-    setList(listObj)
-    
-    console.log()
   }
 
   return (
@@ -108,7 +91,7 @@ function Home() {
 
         <div className={style.numbers}>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Control type="number" placeholder="Digite quantos numeros deseja: " onChange={e => setQuantity(e.target.value)} />
+            <Form.Control type="number" placeholder="Digite quantas linhas: " onChange={e => setQuantity(e.target.value)} />
           </Form.Group>
           <Button type="submit" onClick={() => ListRender(quantity)}>Gerar Lista</Button>
         </div>
@@ -117,7 +100,8 @@ function Home() {
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
             <Form.Control type="text" placeholder="Selecione um numero: " onChange={e => setSelected(e.target.value)} />
           </Form.Group>
-          <Button type="submit" onClick={() => SelectNumber(selectedNum)}>Selecionar Numero</Button>
+          {/* <Button className={style.button} type="submit" onClick={() => SelectNumber(selectedNum)}>Sel e ord por Posição</Button> */}
+          <Button className={style.button} type="submit" onClick={() => SelectNumber(selectedNum)}>Select Number</Button>
         </div>
 
         <div className={style.numbers}>
@@ -133,6 +117,7 @@ function Home() {
             <th>Posição</th>
             <th>Linha</th>
             <th>Repetido</th>
+            <th>Não Repetido</th>
             <th>Numero</th>
             <th>Porcentagem</th>
           </tr>
